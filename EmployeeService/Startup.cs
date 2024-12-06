8af971e715de40a30432e6dbdd347220bfed6b0c
gookian <bezginov.daniil@mail.ru>
2024-12-06 13:13:40 +0700

    Инициализация файлов проекта
    Загружены начальные файлы проекта для сервиса EmployeeService.

---
+++ EmployeeService/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace EmployeeService
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
