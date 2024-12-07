abdc2cc8df29654bcbc30fe45e6111b687140422
Daniil <48077576+Gookian@users.noreply.github.com>
2024-12-07 22:28:43 +0700

    Добавление контроллера, атрибутов валидации и интеграции с Swagger
    - Создан контроллер PersonsController для работы с сущностью Person.
    - Добавлены атрибуты валидации для полей Name и DisplayName
    - Интегрирована поддержка Swagger для автоматической генерации документации API.

---
+++ EmployeeService/Validations/AlphanumericOnlyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeService.Validations
+{
+    public class AlphanumericOnlyAttribute : ValidationAttribute
+    {
+        public AlphanumericOnlyAttribute()
+            : base("The field {0} must contain only letters and digits.")
+        {
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null || value is not string)
+            {
+                return false;
+            }
+
+            bool valid = ((string)value)
+                .All(c => char.IsLetterOrDigit(c) && !char.IsPunctuation(c));
+
+            return valid;
+        }
+    }
+}
---
+++ EmployeeService/Validations/SymbolOnlyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeService.Validations
+{
+    public class SymbolOnlyAttribute : ValidationAttribute
+    {
+        public SymbolOnlyAttribute()
+            : base("The field {0} must contain only characters.")
+        {
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null || value is not string)
+            {
+                return false;
+            }
+
+            bool valid = ((string)value)
+                .All(c => !char.IsDigit(c) && !char.IsPunctuation(c));
+
+            return valid;
+        }
+    }
+}
